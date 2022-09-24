b9c0017a1fb50021bb0a29772b65abf896d68987
Shanejiang1 <jiangshanghang@gmail.com>
2022-09-24 21:44:21 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Shane/>
+</template>
+
+<script>
+import Shane from './components/Shane.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Shane
+  }
+}
+</script>
