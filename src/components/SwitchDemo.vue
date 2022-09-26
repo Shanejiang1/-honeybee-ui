bb060193bebaeb373a022fb107be72a4f578d8ec
Shanejiang1 <jiangshanghang@gmail.com>
2022-09-26 15:03:26 +0800

    switch css 版本1

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    SwitchDemo
+    <Switch />
   </div>
 </template>
+
+<script lang='ts'>
+import Switch from '../lib/Switch.vue'
+
+export default {
+  components: {Switch}
+}
+</script>
