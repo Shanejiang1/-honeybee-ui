0cd5ea6a5479c89f795a08c888f0c3481128dc1e
Shanejiang1 <jiangshanghang@gmail.com>
2022-09-27 14:33:38 +0800

    Button 增加 size 和 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -7,6 +7,38 @@
     <Button theme='link'>你好</Button>
     <Button theme='text'>你好</Button>
   </div>
+  <h1>示例2</h1>
+  <div>
+    <Button>你好</Button>
+    <Button size='big'>你好</Button>
+    <Button size='small'>你好</Button>
+  </div>
+  <div>
+    <Button theme='link'>你好</Button>
+    <Button theme='link' size='big'>你好</Button>
+    <Button theme='link' size='small'>你好</Button>
+  </div>
+  <div>
+    <Button theme='text'>你好</Button>
+    <Button theme='text' size='big'>你好</Button>
+    <Button theme='text' size='small'>你好</Button>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <Button level='main'>主要</Button>
+    <Button>普通</Button>
+    <Button level='danger'>危险</Button>
+  </div>
+  <div>
+    <Button theme='link' level='main'>主要</Button>
+    <Button theme='link'>普通</Button>
+    <Button theme='link' level='danger'>危险</Button>
+  </div>
+  <div>
+    <Button theme='text' level='main'>主要</Button>
+    <Button theme='text'>普通</Button>
+    <Button theme='text' level='danger'>危险</Button>
+  </div>
 </template>
 
 <script lang='ts'>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,19 +1,40 @@
 <template>
   <button
     class='honeybee-button'
-    :class='{[`honeybee-theme-${theme}`]: theme}'
+    :class='classes'
   >
     <slot />
   </button>
 </template>
 
 <script lang='ts'>
+import { computed } from 'vue';
+
 export default {
   props: {
     theme: {
       type: String,
       default: 'button',
     },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    level: {
+      type: String,
+      default: 'normal',
+    },
+  },
+  setup(props) {
+    const { theme, size, level } = props
+    const classes = computed(() => {
+      return {
+        [`honeybee-theme-${theme}`]: theme,
+        [`honeybee-size-${size}`]: size,
+        [`honeybee-level-${level}`]: level,
+      }
+    })
+    return { classes }
   },
 }
 </script>
@@ -24,6 +45,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .honeybee-button {
   box-sizing: border-box;
   height: $h;
@@ -38,13 +60,13 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
   }
 
-  &:hover,
-  &:focus {
+  &:hover, &:focus {
     color: $blue;
     border-color: $blue;
   }
@@ -55,6 +77,90 @@ $radius: 4px;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.honeybee-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.honeybee-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.honeybee-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.honeybee-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.honeybee-theme-button {
+    &.honeybee-level-main {
+      background: $blue;
+      color: #ffffff;
+      border-color: $blue;
+
+      &:hover, &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.honeybee-level-danger {
+      background: $red;
+      border-color: $red;
+      color: #ffffff;
+
+      &:hover, &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.honeybee-theme-link {
+    &.honeybee-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.honeybee-theme-text {
+    &.honeybee-level-main {
+      color: $blue;
+
+      &:hover, &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.honeybee-level-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
