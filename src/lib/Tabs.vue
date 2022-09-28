e03f8af1aa57de3cff6e726ab0f9c6f3c6314202
Shanejiang1 <jiangshanghang@gmail.com>
2022-09-28 18:01:49 +0800

    Tabs 优化 2

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -21,11 +21,8 @@
     </div>
     <div class='honeybee-tabs-content'>
       <component
-        class='honeybee-tabs-content-item'
-        :class='{selected: c.props.title === selected}'
-        v-for='(c, index) in defaults'
-        :is='c'
-        :key='index'
+        :is='current'
+        :key='current.props.title'
       />
     </div>
 
@@ -34,7 +31,7 @@
 
 <script lang='ts'>
 import Tab from './Tab.vue'
-import { computed, onMounted, onUpdated, ref } from 'vue';
+import { computed, onMounted, ref, watchEffect } from 'vue';
 
 export default {
   props: {
@@ -48,18 +45,16 @@ export default {
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
 
-    const x = () => {
-      const { width } = selectedItem.value.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const { left: left1 } = container.value.getBoundingClientRect()
-      const { left: left2 } = selectedItem.value.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-
-    // onMounted 只在第一次渲染执行
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const { left: left1 } = container.value.getBoundingClientRect()
+        const { left: left2 } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
 
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -68,9 +63,7 @@ export default {
       }
     })
     const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
+      return defaults.find(tag => tag.props.title === props.selected)
     })
     const titles = defaults.map((tag) => {
       return tag.props.title
@@ -79,10 +72,10 @@ export default {
       context.emit('update:selected', title)
     }
     return {
+      current,
       defaults,
       titles,
       select,
-      current,
       selectedItem,
       indicator,
       container,
@@ -130,14 +123,6 @@ $border-color: #d9d9d9;
 
   &-content {
     padding: 8px 0;
-
-    &-item {
-      display: none;
-
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
