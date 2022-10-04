622a5daf5371f34b3e991fbd712c6257ce027011
Shanejiang1 <jiangshanghang@gmail.com>
2022-10-04 17:01:28 +0800

    部署脚本 修改

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git checkout -M master &&
-git remote add origin git@github.com:Shanejiang1/honeybee-ui-website.git &&
+git branch -M master &&
+# GitHub
+# git remote add origin git@github.com:Shanejiang1/honeybee-ui-website.git &&
+# 码云
+git remote add origin git@gitee.com:shanejiang/honeybee-ui-website.git &&
 git push -f -u origin master &&
 cd -
